f1d4ab25a4da605c987c0151a6d9648640913a65
SenpaiSun <www.maxxi@mail.ru>
2023-02-09 21:42:26 +0600

    complete default card

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,40 @@
-let buttonProfile = document.querySelector('.profile__info-edit');
-let buttonClose = document.querySelector('.popup__close');
-let popup = document.querySelector('.popup');
-let userName = document.querySelector('.profile__info-username');
-let userNote = document.querySelector('.profile__info-note');
-let nameInput = document.querySelector('.popup__name');
-let noteInput = document.querySelector('.popup__note');
-let buttonSave = document.querySelector('.popup__save');
+const buttonProfile = document.querySelector('.profile__info-edit');
+const buttonClose = document.querySelector('.popup__close');
+const popup = document.querySelector('.popup');
+const userName = document.querySelector('.profile__info-username');
+const userNote = document.querySelector('.profile__info-note');
+const nameInput = document.querySelector('.popup__name');
+const noteInput = document.querySelector('.popup__note');
+const buttonSave = document.querySelector('.popup__save');
+const cardTemplate = document.querySelector('#cardTemlate').content;
+const sectionContent = document.querySelector('.content');
+
+const saveCard = [
+  {
+    name: 'Долина Смерти',
+    link: 'https://lingua-airlines.ru/wp-content/uploads/2021/01/dolina_smerti.jpg'
+  },
+  {
+    name: 'Эрмитаж',
+    link: 'https://puteshestvyie.ru/sites/default/files/sankt-peterburg-2.jpg'
+  },
+  {
+    name: 'Великая Китайская стена',
+    link: 'https://img3.akspic.ru/attachments/originals/2/9/3/4/0/104392-fortifikaciya-gora-chudesa_mira-gornaya_stanciya-nebo-3071x2047.jpg'
+  },
+  {
+    name: 'Сеульский лес',
+    link: 'https://drbenjaminhabib.files.wordpress.com/2015/06/06_sculpture-at-seoul-forest.jpg'
+  },
+  {
+    name: 'Банф',
+    link: 'https://sportishka.com/uploads/posts/2022-03/1646246609_58-sportishka-com-p-banf-kanada-turizm-krasivo-foto-64.jpg'
+  },
+  {
+    name: 'Тодай-дзи',
+    link: 'http://pm1.narvii.com/7162/cda3d5d3ed915c10a2faadb9b2291e43455cdcaer1-1200-900v2_uhq.jpg'
+  }
+];
 
 buttonProfile.addEventListener('click', function () {
   popup.classList.remove('popup');
@@ -32,3 +61,14 @@ popup.addEventListener('keydown', function (event) {
       popup.setAttribute('style', 'display: none;')
     }
 })
+
+function defaultCard(array) {
+  array.map((item) => {
+    const cardTemplateCopy = cardTemplate.querySelector('.content__card').cloneNode(true);
+    cardTemplateCopy.querySelector('.content__card-name').textContent = item.name;
+    cardTemplateCopy.querySelector('.content__card-photo').src = item.link;
+    cardTemplateCopy.querySelector('.content__card-photo').alt = item.name;
+    sectionContent.prepend(cardTemplateCopy)
+  })
+}
+defaultCard(saveCard)
