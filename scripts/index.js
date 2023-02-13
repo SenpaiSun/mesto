c3ab07492b7955f4e891efe7e70b47b6ea3af816
SenpaiSun <www.maxxi@mail.ru>
2023-02-13 08:55:39 +0600

    complete feature

--- scripts/index.js
+++ scripts/index.js
@@ -11,57 +11,60 @@ const sectionContent = document.querySelector('.content');
 const popupAddCard = document.querySelector('.popup_add_card');
 const addCardButton = document.querySelector('.profile__added');
 const buttonCloseAddCard = popupAddCard.querySelector('.popup__close')
-const buttonCreateCard = popupAddCard.querySelector('.popup__save');
 
+// Стартовый массив данных карточек
 const saveCard = [
   {
     name: 'Долина Смерти',
-    link: 'https://lingua-airlines.ru/wp-content/uploads/2021/01/dolina_smerti.jpg'
+    link: 'https://sarah.lidbom.com/wp-content/uploads/2018/05/IMG_8950.jpg'
   },
   {
     name: 'Эрмитаж',
-    link: 'https://puteshestvyie.ru/sites/default/files/sankt-peterburg-2.jpg'
+    link: 'https://machinami.biz/wp-content/uploads/2016/01/P8060080.jpg'
   },
   {
     name: 'Великая Китайская стена',
-    link: 'https://img3.akspic.ru/attachments/originals/2/9/3/4/0/104392-fortifikaciya-gora-chudesa_mira-gornaya_stanciya-nebo-3071x2047.jpg'
+    link: 'https://i.pinimg.com/736x/16/f3/d2/16f3d298fb82f801938aa5c87537313f.jpg'
   },
   {
     name: 'Сеульский лес',
-    link: 'https://drbenjaminhabib.files.wordpress.com/2015/06/06_sculpture-at-seoul-forest.jpg'
+    link: 'https://images.unsplash.com/photo-1629137441025-c4b23def8bdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2VvdWwlMjBwbGF6YXxlbnwwfHwwfHw%3D&w=1000&q=80'
   },
   {
     name: 'Банф',
-    link: 'https://sportishka.com/uploads/posts/2022-03/1646246609_58-sportishka-com-p-banf-kanada-turizm-krasivo-foto-64.jpg'
+    link: 'https://img2.goodfon.ru/original/768x1024/0/14/kanada-moraine-lake-banff.jpg'
   },
   {
     name: 'Тодай-дзи',
-    link: 'http://pm1.narvii.com/7162/cda3d5d3ed915c10a2faadb9b2291e43455cdcaer1-1200-900v2_uhq.jpg'
+    link: 'https://i.redd.it/kgf9301mu5k01.jpg'
   }
 ];
 
 // Функция на открытие попапа
-function openPopap(item) {
+function openPopup(item) {
+  document.querySelector('.popup').setAttribute('style', 'transition: visibility 0.3s, opacity 0.4s linear;');
   item.classList.remove('popup');
   item.classList.add('popup__close-active');
 }
 
 // Функция на закрытие попапа
-function closePopap(item) {
+function closePopup(item) {
+  document.querySelector('.popup').setAttribute('style', 'transition: visibility 0.3s, opacity 0.4s linear;')
+  document.querySelector('.popup_add_card').setAttribute('style', 'transition: visibility 0.3s, opacity 0.4s linear;')
   item.classList.remove('popup__close-active');
   item.classList.add('popup');
 }
 
 // Обработчик на кнопку открытия попапа редактирования профиля
 buttonProfile.addEventListener('click', function () {
-  openPopap(popupProfile)
+  openPopup(popupProfile)
   nameInput.value = userName.textContent;
   noteInput.value = userNote.textContent;
 });
 
 // Обработчик на кнопку закрытия попапа редактирования профиля
 buttonCloseProfile.addEventListener('click', function () {
-  closePopap(popupProfile)
+  closePopup(popupProfile)
 });
 
 // Обработчик на кнопку сохранения профиля
@@ -72,12 +75,12 @@ buttonSaveProfile.addEventListener('click', function eventSave (event) {
   popupProfile.classList.remove('popup__close-active');
 })
 
-// Обработчик на нажатие клавиши 'Enter'
+// Обработчик на нажатие клавиши 'Enter' у попапа изменения профиля
 popupProfile.addEventListener('keydown', function (event) {
     if (event.key == 'Enter') {
       userName.textContent = nameInput.value;
       userNote.textContent = noteInput.value;
-      popupProfile.setAttribute('style', 'display: none;')
+      closePopup(popupProfile)
     }
 })
 
@@ -87,49 +90,84 @@ const like = (evt) => {
   evt.target.classList.toggle('content__card-like-active')
 }
 
+// Создаем функцию кнопки delete
 const deleteCard = (evt) => {
   evt.target.closest('.content__card').remove()
 }
 
-// Функция на добавление карточек, с обработчиком функции like и delete
-function defaultCard(array) {
+// Активация-дезактивация у попапа открытия изображения
+const openPopupImage = () => {
+  const popupImageActive = document.querySelector('.popup_open_image-class');
+  popupImageActive.classList.toggle('popup_open_image')
+  popupImageActive.classList.toggle('popup_open_image-active')
+}
+
+
+
+// Функция на добавление карточек, с обработчиком функции like и delete, открытием изображение во весь экран
+function renderCard(array) {
   array.map((item) => {
+    // Записываем значение элемента массива в переменную
+    const arrayItem = item
+    // Делаем копию template элемента
     const cardTemplateCopy = cardTemplate.querySelector('.content__card').cloneNode(true);
+    // Меняем данные у template на данные из элемента массива
     cardTemplateCopy.querySelector('.content__card-name').textContent = item.name;
     cardTemplateCopy.querySelector('.content__card-photo').src = item.link;
     cardTemplateCopy.querySelector('.content__card-photo').alt = item.name;
+    // Добавляем карточку в template элемент
     sectionContent.prepend(cardTemplateCopy);
-    const cardPage = document.querySelector('.content__card');
-    const buttonLike = cardPage.querySelector('.content__card-like');
-    const buttonDeleteCard = cardPage.querySelector('.content__card-delete');
-    buttonLike.addEventListener('click', like);
-    buttonDeleteCard.addEventListener('click', deleteCard);
+    // Вешаем обработчик на лайк, удаление карточки
+    document.querySelector('.content__card-like').addEventListener('click', like)
+    document.querySelector('.content__card-delete').addEventListener('click', deleteCard);
+    // Вешаем обработчик на открытие изображения во весь экран. Добавляю transition для дальнейшего плавного закрытия элемента
+    document.querySelector('.content__card-photo').addEventListener('click', () => {
+      document.querySelector('.popup__text').textContent = item.name;
+      document.querySelector('.popup__full-image').src = item.link;
+      document.querySelector('.popup_open_image').setAttribute('style', 'transition: visibility 0.3s, opacity 0.4s linear;')
+      openPopupImage()
+    })
+    // Вешаем обработчик на закрытие изображения во весь экран
+    document.querySelector('.popup__close-image').addEventListener('click', openPopupImage)
   })
 }
+
 // Вызов функции добавления карточек
-defaultCard(saveCard)
+renderCard(saveCard)
 
 // Обработчик на кнопку добавления нового места
 addCardButton.addEventListener('click', function() {
-  openPopap(popupAddCard)
+  openPopup(popupAddCard)
 })
 
 // Обработчик на кнопку закрытия попапа добавления нового места
 buttonCloseAddCard.addEventListener('click', function() {
-  closePopap(popupAddCard)
+  closePopup(popupAddCard)
 })
 
-// Обработчик на отправку формы и добавления карточки в ленту
+
+// Обработчик на отправку формы и добавления карточки в ленту + очистка формы после отправки
 popupAddCard.addEventListener('submit', (item) => {
   item.preventDefault()
   const cardName = popupAddCard.querySelector('.popup__name').value
   const cardLink = popupAddCard.querySelector('.popup__note').value
   const arrayItem = [{ name: cardName, link: cardLink}]
-  defaultCard(arrayItem)
-  closePopap(popupAddCard)
+  renderCard(arrayItem)
+  closePopup(popupAddCard)
+  popupAddCard.querySelector('.popup__name').value = ''
+  popupAddCard.querySelector('.popup__note').value = ''
 })
 
-
-
-
+// Обработчик на нажатие клавиши 'Enter' у попапа изменения профиля
+popupAddCard.addEventListener('keydown', function (event) {
+  if (event.key == 'Enter') {
+    const cardName = popupAddCard.querySelector('.popup__name').value
+    const cardLink = popupAddCard.querySelector('.popup__note').value
+    const arrayItem = [{ name: cardName, link: cardLink}]
+    renderCard(arrayItem)
+    closePopup(popupAddCard)
+    popupAddCard.querySelector('.popup__name').value = ''
+    popupAddCard.querySelector('.popup__note').value = ''
+  }
+})
 
