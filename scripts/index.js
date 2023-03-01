697bf1b132779a144f27cfb53c85e8c147618b66
SenpaiSun <www.maxxi@mail.ru>
2023-03-01 21:31:31 +0600

    fix problem

--- scripts/index.js
+++ scripts/index.js
@@ -17,34 +17,6 @@ const popupImage = document.querySelector('.popup_open_image')
 const popupImageText = document.querySelector('.popup__text');
 const popupImageOpened = document.querySelector('.popup__full-image');
 
-// Стартовый массив данных карточек
-const cardDefault = [
-  {
-    name: 'Долина Смерти',
-    link: 'https://sarah.lidbom.com/wp-content/uploads/2018/05/IMG_8950.jpg'
-  },
-  {
-    name: 'Эрмитаж',
-    link: 'https://machinami.biz/wp-content/uploads/2016/01/P8060080.jpg'
-  },
-  {
-    name: 'Великая Китайская стена',
-    link: 'https://i.pinimg.com/736x/16/f3/d2/16f3d298fb82f801938aa5c87537313f.jpg'
-  },
-  {
-    name: 'Сеульский лес',
-    link: 'https://images.unsplash.com/photo-1629137441025-c4b23def8bdd?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NHx8c2VvdWwlMjBwbGF6YXxlbnwwfHwwfHw%3D&w=1000&q=80'
-  },
-  {
-    name: 'Банф',
-    link: 'https://img2.goodfon.ru/original/768x1024/0/14/kanada-moraine-lake-banff.jpg'
-  },
-  {
-    name: 'Тодай-дзи',
-    link: 'https://i.redd.it/kgf9301mu5k01.jpg'
-  }
-];
-
 // Функция, которая ищет popup по селектору и вызывает функцию закрытия popup
 const closeEscapeKey = (evt) => {
   if (evt.key === 'Escape') {
